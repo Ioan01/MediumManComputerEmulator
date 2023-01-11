a8ceb0dd3f153d3a8749485922aaf09f252eb9bc
cineicaly <darius.cipleu@student.upt.ro>
2023-01-11 03:02:13 +0200

    Input Validation.

--- BMC_Emulator/Program.cs
+++ BMC_Emulator/Program.cs
@@ -1,13 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using BMC_Emulator;
 using BMC_Emulator.Instructions;
 
 var emulator = new Emulator();
-//Change path to your path (*^^*)
-string[] instructions = File.ReadAllLines("E:\\Users\\wotp3\\Source\\Repos\\MediumManComputerEmulator\\BMC_Emulator\\instructions.txt");
+
+string[] instructions = File.ReadAllLines("..\\..\\..\\instructions.txt");
 instructions = instructions
     .Where(x => !x.Trim().StartsWith("//")) // Removes lines that start with //
     .Where(x => !string.IsNullOrWhiteSpace(x)) // Removes empty lines
@@ -16,12 +15,33 @@ instructions = instructions
 
 // Check for invalid registers
 var invalidRegister = instructions
-    .SelectMany(line => Regex.Matches(line, @"(?i)R[1-9][0-9]+"))
+    .SelectMany(line => Regex.Matches(line, @"R([2-9][0-9]*|1[0-9]+)"))
     .FirstOrDefault();
 
 if (invalidRegister != null)
     throw new Exception($"Invalid register: {invalidRegister.Value}");
 
+foreach (string i in instructions)
+{
+    string pattern = @"^(STR|LDR)";
+    Regex rgx = new Regex(pattern);
+    Match match = rgx.Match(i);
+    bool exist_R = match.Success;
+
+    if (exist_R)
+    {
+        string pattern2 = @",([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]|R[01])\b";
+        Regex rgx2 = new Regex(pattern2);
+        Match match2 = rgx2.Match(i);
+        bool exist = match2.Success;
+        if (!exist)
+        {
+            throw new Exception($"Invalid value!");
+        }
+    }
+
+    //throw new Exception($"Invalid value!");
+}
 
 emulator.LoadProgram(instructions);
 emulator.Run();
